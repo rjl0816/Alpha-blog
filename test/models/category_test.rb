04158d5f2b22205892238e64a19e89d5292bea93
Lotino Ron Jefferson <lotino@barnet.co.jp>
2021-07-08 18:36:46 +0900

    add unit tests for category model

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,9 +2,33 @@ require 'test_helper'
 
 class Categorytest < ActiveSupport::TestCase
 
-    test "Category should be valid" do 
+    def setup
         @category = Category.new(name: "Sports")
+    end
+
+    test "Category should be valid" do 
         assert @category.valid?
+    end
+
+    test "Name should be present" do 
+        @category.name = " "
+        assert_not @category.valid?
+    end
+
+    test "Name should be unique" do 
+        @category.save
+        @category2 = Category.new(name: "Sports")
+        assert_not @category2.valid?
+    end
+
+    test "Name should not be too long" do 
+        @category.name = "a" * 30
+        assert_not @category.valid?
+    end
+
+    test "Name should not be too short" do 
+        @category.name = "aa"
+        assert_not @category.valid?
     end
 
 end
