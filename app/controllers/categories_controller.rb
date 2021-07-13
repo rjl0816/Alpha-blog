e7277174672f7df28c231b0066a58c359ab73f41
Lotino Ron Jefferson <lotino@barnet.co.jp>
2021-07-13 20:14:54 +0900

    add edit update destroy action for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,13 @@
 class CategoriesController < ApplicationController
 
     before_action :require_admin, except: [:index, :show]
+    before_action :set_category, only: [:show, :edit, :update, :destroy]
 
     def index
         @categories = Category.paginate(page: params[:page], per_page: 5)
     end
 
     def show 
-        @category = Category.find(params[:id])
         @articles = @category.articles.paginate(page: params[:page], per_page: 3)
     end
 
@@ -25,7 +25,28 @@ class CategoriesController < ApplicationController
         end
     end
 
+    def edit 
+
+    end
+
+    def update 
+        if @category.update(category_params)
+            flash[:notice] = "Category name updated successfully"
+            redirect_to @category
+        else 
+            render 'edit'
+        end
+    end
+
+    def destroy
+
+    end
+
     private
+
+    def set_category
+        @category = Category.find(params[:id])
+    end
 
     def category_params
         params.require(:category).permit(:name)
